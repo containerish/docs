a7014d46c4938a02954051bdca321885473ab85a
guacamole <gunjanwalecha@gmail.com>
2022-04-06 23:07:35 +0530

    Fix: logo was blurred, changed blog to documentaion
    Signed-off-by: guacamole <gunjanwalecha@gmail.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,14 +3,14 @@ module.exports = {
   base: '/',
   head: [['link', { rel: 'icon', href: 'https://raw.githubusercontent.com/containerish/docs/main/assets/favicon.png'}]],
   lang: 'en-US',
-  title: "Blog",
+  title: "Documentation",
   description: "A decentralised container registry fully compliant with OCI Distribution Specification",
 
   themeConfig: {
     darkMode: true,
     repo: 'https://github.com/containerish',
     editLink: false,
-    logo: 'https://raw.githubusercontent.com/containerish/docs/main/assets/logo.png',
+    logo: 'https://app.openregistry.dev/logo.svg',
     contributors: true,
     sidebar: [
       {
