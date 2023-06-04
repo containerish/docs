164dffec2b1c761d97c01e3e4313deb3724e81b7
jay-dee7 <me@jsdp.dev>
2023-06-04 12:23:47 +0530

    add: Heading auto link generation using rehype

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,19 +1,29 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import toc from '@jsdevtools/rehype-toc';
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md'],
-	layout:{
+	layout: {
 		post: './src/lib/postLayout.svelte'
 	},
-
 	smartypants: {
 		dashes: 'oldschool'
 	},
-
-	remarkPlugins: [
-		
-	],
-	rehypePlugins: []
+	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+	rehypePlugins: [
+		rehypeSlug,
+		[
+			rehypeAutolinkHeadings,
+			{
+				behavior: 'wrap'
+			}
+		],
+		toc
+	]
 });
 
 export default config;
